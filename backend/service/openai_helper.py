f1ba6784d0982e39d8801b43abfbd95d0c26d003
pengfei.miao <pengfei.miao@thoughtworks.com>
2023-11-16 23:45:09 +0800

    implement auth

--- backend/service/openai_helper.py
+++ backend/service/openai_helper.py
@@ -10,12 +10,11 @@ class OpenAIHelper:
     def __init__(self):
         try:
             self.model = "gpt-3.5-turbo"
-            self.client = None
-            # OpenAI(
-            #     organization=OPENAI_ORG,
-            #     api_key=OPENAI_KEY
-            # )
-            # print(self.client.models.list())
+            self.client = OpenAI(
+                organization=OPENAI_ORG,
+                api_key=OPENAI_KEY
+            )
+            print(self.client.models.list())
         except Exception as e:
             logging.error(f"Failed to Connect OpenAI: {e}")
             # sys.exit(1)
